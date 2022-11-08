86c46d07b7088d2045e9e16ed9056bc5eddb9452
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2022-11-09 02:26:28 +0700

    add Axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import axios from 'axios'
+
 // import './assets/main.css'
 import '@/assets/main.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 
 createApp(App).mount('#app')
+app.config.globalProperties.axios = axios
