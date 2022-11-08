f8bd7e609161c9bc2e780781e333600d6cfefbc6
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2022-11-09 02:22:07 +0700

    setup boostratp & sass

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+// import './assets/main.css'
+import '@/assets/main.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 createApp(App).mount('#app')
